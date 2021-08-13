061c77fbbe9031994e0f379a58801ccf719b5bab
sekiguchi0215 <dsphere5296@gmail.com>
2021-08-13 22:33:55 +0900

    動画データの検索処理を記述

--- youtube_search.rb
+++ youtube_search.rb
@@ -4,3 +4,7 @@ require "google/apis/youtube_v3"
 youtube = Google::Apis::YoutubeV3::YouTubeService.new
 
 youtube.key = ENV["GOOGLE_API_KEY"]
+
+youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: "ポケモンカード", max_results: 10)
+
+puts youtube_search_list
